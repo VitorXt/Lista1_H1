2f729c32cfce8190622b798ef75e256e8c00d1ba
VitorHRV <62753727+VitorXt@users.noreply.github.com>
2023-08-20 14:41:47 -0300

    Add files via upload

---
+++ Pratica/Pratica/Models/Request/NovoAlunoViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Pratica.Models.Request
+{
+    public class NovoAlunoViewModel
+    {
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public string CPF { get; set; }
+        public bool Ativo { get; set; }
+    }
+}
